59f19f63b0ea9b6b9d4df773fa32879b372c8c61
Antonio Murabito <Thepro007002001@gmail.com>
2026-01-08 23:17:16 +0100

    Update ecosystem.config.js

--- Code/ecosystem.config.js
+++ Code/ecosystem.config.js
@@ -5,13 +5,6 @@ module.exports = {
       script: "server.ts", // Il file da avviare
       cwd: "./backend/api/src/", // La cartella dove si trova questo server
       interpreter: "/home/kirk/.bun/bin/bun", // Percorso assoluto di Bun (verifica con 'which bun')
-      env: {
-        PORT: 6969,
-        DATABASE_URL: process.env.DATABASE_URL,
-        BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-        BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
-        API_KEY_SECRET: process.env.API_KEY_SECRET,
-      },
     },
   ],
 };
