fa26a46bd339154f0503908869b710762662ea76
Antonio Murabito <Thepro007002001@gmail.com>
2026-01-08 23:19:41 +0100

    Update ecosystem.config.js

--- Code/ecosystem.config.js
+++ Code/ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: "kaffettino-api-server", // Nome del processo in PM2
       script: "server.ts", // Il file da avviare
       cwd: "./backend/api/src/", // La cartella dove si trova questo server
+      args: "run", // Argomenti da passare allo script
       interpreter: "/home/kirk/.bun/bin/bun", // Percorso assoluto di Bun (verifica con 'which bun')
     },
   ],
